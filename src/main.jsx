8fff7dd40aa80ee93b4bfdf5bb8f3c0b6c08b78b
ChinniSaiteja <chinnisaiteja8688@gmail.com>
2024-03-21 16:16:35 +0530

    sai

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+import BrowserRouter from 'react-router-dom';
 
 const Home = () => {
   return <h1>Routing</h1>;
-};
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
